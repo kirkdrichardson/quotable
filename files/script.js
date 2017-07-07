27ea18c9a3bfe04a3af7d46a34ea46a51d1cd295
Kirk <kirk.douglas.richardson@gmail.com>
2017-07-07 15:10:52 -0600

    update 07/06/17

--- files/script.js
+++ files/script.js
@@ -1,13 +1,55 @@
 
+
+
 $(document).ready(function(){
-  var endpoint = "http://quotes.stormconsultancy.co.uk/random.json";
+  getQuote(); // loads a new quote by default
+  // new quote when "..." button is pressed
   $("#quoteBtn").click(function() {
+    getQuote();
+  });
+  // prepares a quote to be posted as a tweet
+  $("#tweetBtn").click(function() {
+    twitterQuery();
+  })
+  //#######################################//
+  // ####    Function Definitions    #### //
+  //#####################################//
+
+  // queries the API (endpoint) for a new quote & wraps its properties in <p> elements
+  function getQuote() {
+    var endpoint = "http://quotes.stormconsultancy.co.uk/random.json";
     $.getJSON(endpoint, function(json) {
       $(".text").html(
-        json.quote + "<p class='name'>- " + (json.author)+ "</p>")
-      });
+        "<p class='quote'>" + json.quote + "</p>" +
+        "<p class='name'>- " + json.author + "</p>"
+      )
     });
-  });
+  }
+
+  // returns a url for a Twitter post
+  // Known issue: not all extended ASCII char encode correctly (e.g. ++ in "C++")
+  // a ; will pass through the encodeURI function & cut short the tweet
+  function twitterQuery(){
+    var query;
+    var base = "https://twitter.com/intent/tweet?text=";
+    var quote = $(".quote").text();
+    var name = $(".name").text();
+    var endBit = (140 - (name.length + 6));
+    if ((quote.length + name.length) >= 138) {
+      quote = quote.slice(0, endBit);
+      query = ('"' + quote + '..." ' + name);
+    }
+    else {
+      query = ('"' + quote + '" ' + name);
+    }
+    // var query = encodeURI(quote + " " + name);
+    // var tweet = base + query;
+    window.open(base + encodeURI(query));
+  }
+
+});
+
+
 
 
 
